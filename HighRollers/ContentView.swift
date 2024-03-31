5fdd7c9c9a8817529610eb0c8281f3ab89510b99
Daniel Freire <dgz1606@gmail.com>
2024-03-31 11:35:58 -0500

    add rolls animation and support for color scheme

--- HighRollers/ContentView.swift
+++ HighRollers/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
 
     @State private var currentResult = DiceResult(type: 0, number: 0)
 
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State private var stoppedDice = 0
+
     let columns: [GridItem] = [
         .init(.adaptive(minimum: 60))
     ]
@@ -39,22 +42,38 @@ struct ContentView: View {
                             Text(String(currentResult.rolls[rollNumber]))
                                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                                 .aspectRatio(1, contentMode: .fit)
-                                .foregroundStyle(.white)
-                                .background(.black)
+                                .foregroundStyle(Color.primary)
+                                .background(.background)
                                 .clipShape(.rect(cornerRadius: 10))
-                                .shadow(color: .white, radius: 3)
+                                .shadow(color: Color.primary, radius: 3)
                                 .font(.title)
                                 .padding(5)
                         }
                     }
                 }
+                .disabled(stoppedDice < currentResult.rolls.count)
             }
             .navigationTitle("High Rollers")
+            .onReceive(timer) { _ in
+                updateDice()
+            }
         }
     }
 
     func rollDice() {
         currentResult = DiceResult(type: selectedDiceType, number: numberToRoll)
+        stoppedDice = -20
+    }
+
+    func updateDice() {
+        guard stoppedDice < currentResult.rolls.count else { return }
+
+        for i in stoppedDice ..< numberToRoll {
+            if i < 0 { continue }
+            currentResult.rolls[i] = Int.random(in: 1 ... selectedDiceType)
+        }
+
+        stoppedDice += 1
     }
 }
 
