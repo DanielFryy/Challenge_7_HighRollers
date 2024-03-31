6ffb19284083e88c92c0d3a8b0e5406ebd79efcb
Daniel Freire <dgz1606@gmail.com>
2024-03-31 10:44:54 -0500

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Daniel Freire on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
