7f2190e2e93616aec4f9dd0bc751dcc1df94b65f
Daniel Freire <dgz1606@gmail.com>
2024-03-31 10:56:57 -0500

    add basic structure to roll dices

---
+++ HighRollers/DiceResult.swift
@@ -0,0 +1,25 @@
+//
+//  DiceResult.swift
+//  HighRollers
+//
+//  Created by Daniel Freire on 3/31/24.
+//
+
+import Foundation
+
+struct DiceResult: Codable, Identifiable {
+    var id = UUID()
+    var type: Int
+    var number: Int
+    var rolls = [Int]()
+
+    init(type: Int, number: Int) {
+        self.type = type
+        self.number = number
+
+        for _ in 0 ..< number {
+            let roll = Int.random(in: 1 ... type)
+            rolls.append(roll)
+        }
+    }
+}
